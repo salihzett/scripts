f8e5793f062f04a0b8ab918214a86fdc219ca619
salihzett <salihzett@users.noreply.github.com>
2017-03-12 16:45:07 +0100

    Update config_ard.sh

--- deployscripts/config_ard.sh
+++ deployscripts/config_ard.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #access for admin and set request
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users admin -privs -all -clientopts -setreqperm -reqperm yes -restart -agent
+echo "Config ARD"
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users admin -privs -all -clientopts -setmenuextra -menuextra no -setreqperm -reqperm yes -restart -agent 
