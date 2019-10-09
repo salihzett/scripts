7463d59081c31a3762ac6b4e599d0c81b1f033f4
salihzett <salihzett@users.noreply.github.com>
2019-10-09 16:31:11 +0200

    Delete hwid.ps1

--- windows/hwid.ps1
+++
@@ -1,6 +0,0 @@
-md c:\\HWID
-Set-Location c:\\HWID
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
-Install-Script -Name Get-WindowsAutoPilotInfo 
-$b = hostname
-Get-WindowsAutoPilotInfo.ps1 -OutputFile c:\$b.csv
