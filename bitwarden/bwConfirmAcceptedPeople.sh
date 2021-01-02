062dbe9a517775375d5d658e5550bc58b612985f
salihzett <salihzett@users.noreply.github.com>
2021-01-02 18:01:49 +0100

    Update bwConfirmAcceptedPeople.sh

--- bitwarden/bwConfirmAcceptedPeople.sh
+++ bitwarden/bwConfirmAcceptedPeople.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+PATH="/usr/local/bin:/usr/bin:/bin"
 
-read -p 'Organization Id: ' organization_id
-session_key="$(bw unlock --raw)"
+organization_id="ORG ID"
+session_key="SESSION KEY"
 org_members="$(bw list --session $session_key org-members --organizationid $organization_id | jq -c '.[] | select( .status == 1 )' | jq -c '.id' | tr -d '"')"
 for member_id in ${org_members[@]} ; do
-	bw confirm --session $session_key org-member $member_id --organizationid $organization_id
+        /usr/local/bin/bw confirm --session $session_key org-member $member_id --organizationid $organization_id
 done
