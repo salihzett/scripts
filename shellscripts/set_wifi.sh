73cbec2aa37b09b4f7b79dfb0dd3df685d4712c7
salihzett <salihzett@users.noreply.github.com>
2021-02-16 22:52:07 +0100

    Update set_wifi.sh

--- shellscripts/set_wifi.sh
+++ shellscripts/set_wifi.sh
@@ -2,3 +2,6 @@
 echo "####### join Wifi #######"
 /usr/sbin/networksetup networksetup -setairportnetwork en0 Wifi "passphrase"
 echo "####### FINISHED #######"
+
+exit 0 ## Success
+exit 1 ## Failure
