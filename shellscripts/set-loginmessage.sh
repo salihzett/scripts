6c500757f7e55ae7c993e50aecace37e1b1d41de
salihzett <salihzett@users.noreply.github.com>
2021-02-17 10:18:48 +0100

    Update set-loginmessage.sh

--- shellscripts/set-loginmessage.sh
+++ shellscripts/set-loginmessage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Login Message
-/usr/bin/defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "logintext"
+/usr/bin/defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "Company | Street | Melbourne, VIC"
 
 exit 0
 exit 1
