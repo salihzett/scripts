2487a6f8d778e1c61f78f70a440b9f39853fe9e3
salihzett <salihzett@users.noreply.github.com>
2023-01-07 14:01:09 +0100

    Create getBatteryCycleCount.sh

---
+++ intune/macOS/Custom attributes/getBatteryCycleCount.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#set -x
+
+############################################################################################
+##
+## Extension Attribute script to return Battery Cycle Count
+##
+############################################################################################
+
+battery=$(system_profiler SPPowerDataType | grep "Cycle Count" | awk '{print $3}'	)
+echo $battery
